baa3c0abffed00ee5e16b6353186b6de3a99f1aa
“KEEVIN <keevin.mitchell@gmail.com>
2014-11-04 08:18:42 -0500

    Initial Commit

---
+++ SpotPullDowenSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpotPullDowenSwift
+//
+//  Created by KEEVIN MITCHELL on 11/4/14.
+//  Copyright (c) 2014 Beyond 2021. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
